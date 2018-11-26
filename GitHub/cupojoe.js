89998a45ea83e0757b12bd0da34f9ce1bc58a3fa
DeChozenWhone95 <45363157+DeChozenWhone95@users.noreply.github.com>
2018-11-26 11:46:56 -0600

    Add files via upload

---
+++ GitHub/cupojoe.js
@@ -0,0 +1,21 @@
+const danny =()=> {
+    let gibson = document.getElementById('div2');
+let p = document.createElement('p');
+p.innerText = 'I am getting too old for this!!!';
+gibson.appendChild(p);
+}
+
+const beam = () => {
+    let scotty = document.getElementById('beam1');
+    scotty.style.color = "blue";
+}
+
+const lala = () => {
+    let lal = document.getElementById('lal1');
+    lal.style.textDecoration= "overline";
+}
+
+const dillhole =()=> {
+    let figgis = document.getElementById('nahh');
+    figgis.classList.add('lolly');
+}
